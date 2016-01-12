9cf22a890ab5d8c3645cd0d72b6b64b53b42930b
geoffviola <geoffrey.viola@asirobots.com>
2016-01-12 16:52:32 -0700

    added capability to override mode instead of force it

--- src/imu_3dm_gx3.cc
+++ src/imu_3dm_gx3.cc
@@ -7,7 +7,7 @@
 #include <tf/tf.h>
 #include <boost/asio.hpp>
 #include <boost/asio/serial_port.hpp>
-#include <eigen3/Eigen/Geometry> 
+#include <eigen3/Eigen/Geometry>
 // #include "pose_utils.h"
 
 using namespace std;
@@ -67,6 +67,8 @@ inline void print_bytes(const unsigned char *data, unsigned short length)
 
 int main(int argc, char** argv)
 {
+  static const uint8_t DEFAULT_SEND_MODE = 0XCCU;
+  static const int MAX_BUFFER_SIZE = 100;
   ros::init(argc, argv, "imu_3dm_gx3");
   ros::NodeHandle n("~");
 
@@ -111,6 +113,22 @@ int main(int argc, char** argv)
   double delay;
   n.param("delay", delay, 0.0);
 
+  std::string send_mode_raw;
+  uint32_t send_mode;
+  const bool set_mode = n.getParam("send_mode", send_mode_raw);
+  if (set_mode)
+    {
+      std::stringstream ss;
+      ss << std::hex << send_mode_raw;
+      ss >> send_mode;
+      ROS_INFO("User mode set to 0x%x", send_mode);
+    }
+  else
+    {
+      send_mode = static_cast<uint32_t>(DEFAULT_SEND_MODE);
+      ROS_INFO("Default mode set to 0x%x", send_mode);
+    }
+
   typedef boost::asio::serial_port_base sb;
 
   sb::baud_rate baud_option(baud);
@@ -123,110 +141,126 @@ int main(int argc, char** argv)
   serial_port->set_option(parity);
   serial_port->set_option(stop_bits);
 
-  // Stop continous mode if it is running
-  boost::asio::write(*serial_port, boost::asio::buffer(stop, 3));
-  ROS_WARN("Wait 0.1s"); 
-  ros::Duration(0.1).sleep();
-
-  // Check the mode
-  bool reInitFlag = false;
-  boost::asio::write(*serial_port, boost::asio::buffer(mode, 4));
-  boost::asio::read(*serial_port, boost::asio::buffer(reply, REPLY_LENGTH));
-  if (!validate_checksum(reply, REPLY_LENGTH))
+  if (!set_mode)
     {
-      ROS_ERROR("%s: failed to get mode", name.c_str());
-      if (serial_port->is_open())
-        serial_port->close();
-      reInitFlag = true;
-    }
+      // Stop continous mode if it is running
+      boost::asio::write(*serial_port, boost::asio::buffer(stop, 3));
+      ROS_WARN("Wait 0.1s");
+      ros::Duration(0.1).sleep();
 
-  if (reInitFlag)
-  {
-    ROS_WARN("In Re-Init");
-    ros::Duration(0.1).sleep();
-    try
-      {
-        serial_port->open(port);
-      }
-    catch (boost::system::system_error &error)
-      {
-        ROS_ERROR("%s: Failed to open port %s with error %s",
-                  name.c_str(), port.c_str(), error.what());
-        return -1;
-      }
-    if (!serial_port->is_open())
-      {
-        ROS_ERROR("%s: failed to open serial port %s",
-                  name.c_str(), port.c_str());
-        return -1;
-      }
-    serial_port->set_option(baud_option);
-    serial_port->set_option(flow_control);
-    serial_port->set_option(parity);
-    serial_port->set_option(stop_bits);
-    // Check the mode
-    boost::asio::write(*serial_port, boost::asio::buffer(mode, 4));
-    boost::asio::read(*serial_port, boost::asio::buffer(reply, REPLY_LENGTH));
-    if (!validate_checksum(reply, REPLY_LENGTH))
-      {
-        ROS_ERROR("%s: failed to get mode", name.c_str());
-        if (serial_port->is_open())
-          serial_port->close();
-        return -1;
-      }    
-  }
-
-  // If we are not in active mode, change it
-  if (reply[2] != '\x01')
-    {
-      mode[3] = '\x01';
+      // Check the mode
+      bool reInitFlag = false;
       boost::asio::write(*serial_port, boost::asio::buffer(mode, 4));
       boost::asio::read(*serial_port, boost::asio::buffer(reply, REPLY_LENGTH));
       if (!validate_checksum(reply, REPLY_LENGTH))
         {
-          ROS_ERROR("%s: failed to set mode to active", name.c_str());
+          ROS_ERROR("%s: failed to get mode", name.c_str());
+          if (serial_port->is_open())
+            serial_port->close();
+          reInitFlag = true;
+        }
+
+      if (reInitFlag)
+      {
+        ROS_WARN("In Re-Init");
+        ros::Duration(0.1).sleep();
+        try
+          {
+            serial_port->open(port);
+          }
+        catch (boost::system::system_error &error)
+          {
+            ROS_ERROR("%s: Failed to open port %s with error %s",
+                      name.c_str(), port.c_str(), error.what());
+            return -1;
+          }
+        if (!serial_port->is_open())
+          {
+            ROS_ERROR("%s: failed to open serial port %s",
+                      name.c_str(), port.c_str());
+            return -1;
+          }
+        serial_port->set_option(baud_option);
+        serial_port->set_option(flow_control);
+        serial_port->set_option(parity);
+        serial_port->set_option(stop_bits);
+        // Check the mode
+        boost::asio::write(*serial_port, boost::asio::buffer(mode, 4));
+        boost::asio::read(*serial_port, boost::asio::buffer(reply, REPLY_LENGTH));
+        if (!validate_checksum(reply, REPLY_LENGTH))
+          {
+            ROS_ERROR("%s: failed to get mode", name.c_str());
+            if (serial_port->is_open())
+              serial_port->close();
+            return -1;
+          }
+      }
+
+      // If we are not in active mode, change it
+      if (reply[2] != '\x01')
+        {
+          mode[3] = '\x01';
+          boost::asio::write(*serial_port, boost::asio::buffer(mode, 4));
+          boost::asio::read(*serial_port, boost::asio::buffer(reply, REPLY_LENGTH));
+          if (!validate_checksum(reply, REPLY_LENGTH))
+            {
+              ROS_ERROR("%s: failed to set mode to active", name.c_str());
+              if (serial_port->is_open())
+                serial_port->close();
+              return -1;
+            }
+        }
+
+      // Set the continous preset mode
+      const char preset[4] = {'\xD6','\xC6','\x6B','\xCC'};
+      boost::asio::write(*serial_port, boost::asio::buffer(preset, 4));
+
+      boost::asio::read(*serial_port, boost::asio::buffer(reply, REPLY_LENGTH));
+      if (!validate_checksum(reply, REPLY_LENGTH))
+        {
+          ROS_ERROR("%s: failed to set continuous mode preset", name.c_str());
           if (serial_port->is_open())
             serial_port->close();
           return -1;
         }
+
+      // Set the mode to continous output
+      mode[3] = '\x02';
+      boost::asio::write(*serial_port, boost::asio::buffer(mode, 4));
+      boost::asio::read(*serial_port, boost::asio::buffer(reply, REPLY_LENGTH));
+      if (!validate_checksum(reply, REPLY_LENGTH))
+        {
+          ROS_ERROR("%s: failed to set mode to continuous output", name.c_str());
+          if (serial_port->is_open())
+            serial_port->close();
+          return -1;
+        }
+
+      // Set Timer
+      char set_timer[8] = {'\xD7','\xC1','\x29','\x01','\x00','\x00','\x00','\x00'};
+      unsigned char reply_timer[7];
+      boost::asio::write(*serial_port, boost::asio::buffer(set_timer, 8));
+    //  ros::Time t0 = ros::Time::now();
+      boost::asio::read(*serial_port, boost::asio::buffer(reply_timer, 7));
     }
-
-  // Set the continous preset mode
-  const char preset[4] = {'\xD6','\xC6','\x6B','\xCC'};
-  boost::asio::write(*serial_port, boost::asio::buffer(preset, 4));
-
-  boost::asio::read(*serial_port, boost::asio::buffer(reply, REPLY_LENGTH));
-  if (!validate_checksum(reply, REPLY_LENGTH))
-    {
-      ROS_ERROR("%s: failed to set continuous mode preset", name.c_str());
-      if (serial_port->is_open())
-        serial_port->close();
-      return -1;
-    }
-
-  // Set the mode to continous output
-  mode[3] = '\x02';
-  boost::asio::write(*serial_port, boost::asio::buffer(mode, 4));
-  boost::asio::read(*serial_port, boost::asio::buffer(reply, REPLY_LENGTH));
-  if (!validate_checksum(reply, REPLY_LENGTH))
-    {
-      ROS_ERROR("%s: failed to set mode to continuous output", name.c_str());
-      if (serial_port->is_open())
-        serial_port->close();
-      return -1;
-    }
-
-  // Set Timer
-  char set_timer[8] = {'\xD7','\xC1','\x29','\x01','\x00','\x00','\x00','\x00'};
-  unsigned char reply_timer[7];
-  boost::asio::write(*serial_port, boost::asio::buffer(set_timer, 8));
-//  ros::Time t0 = ros::Time::now();  
-  boost::asio::read(*serial_port, boost::asio::buffer(reply_timer, 7));
   ros::Time t0 = ros::Time::now();  
 
   ROS_WARN("Streaming Data...");
-  unsigned short data_length = 79;
-  unsigned char data[data_length];
+  uint16_t data_length;
+  if (0xCCU == send_mode)
+    {
+      data_length = 79;
+    }
+  else if (0xD3 == send_mode)
+    {
+      data_length = 43;
+    }
+  else
+    {
+      ROS_ERROR("This driver does not support mode \"0x%x\"", send_mode);
+      return EXIT_FAILURE;
+    }
+  unsigned char data[MAX_BUFFER_SIZE];
   sensor_msgs::Imu msg;
   ros::Publisher pub = n.advertise<sensor_msgs::Imu>("imu", 100);
   while (n.ok())
@@ -250,8 +284,11 @@ int main(int argc, char** argv)
         ang_vel[i] = extract_float(&(data[k]));
       for (unsigned int i = 0; i < 3; i++, k += 4)
         mag[i] = extract_float(&(data[k]));
-      for (unsigned int i = 0; i < 9; i++, k += 4)
-        M[i] = extract_float(&(data[k]));
+      if (0xCCU == send_mode)
+        {
+          for (unsigned int i = 0; i < 9; i++, k += 4)
+            M[i] = extract_float(&(data[k]));
+        }
       T = extract_int(&(data[k])) / 62500.0;
 
       msg.header.stamp    = t0 + ros::Duration(T) - ros::Duration(delay);
@@ -262,31 +299,32 @@ int main(int argc, char** argv)
       msg.linear_acceleration.x = acc[0] * 9.81;
       msg.linear_acceleration.y = acc[1] * 9.81;
       msg.linear_acceleration.z = acc[2] * 9.81;
-      // mat R(3,3);
-      // for (unsigned int i = 0; i < 3; i++)
-      //   for (unsigned int j = 0; j < 3; j++)
-      //     R(i,j) = M[j*3+i];
-      // colvec q = R_to_quaternion(R);
-      Eigen::Matrix3d R;
-      for (unsigned int i = 0; i < 3; i++)
-        for (unsigned int j = 0; j < 3; j++)
-          R(i,j) = M[j*3+i];
-      Eigen::Quaternion<double> q(R);
-      msg.orientation.w = (double)q.w();// q(0);
-      msg.orientation.x = (double)q.x();// q(1);
-      msg.orientation.y = (double)q.y();// q(2);
-      msg.orientation.z = (double)q.z();// q(3);
       msg.orientation_covariance[0] = mag[0];
       msg.orientation_covariance[1] = mag[1];
       msg.orientation_covariance[2] = mag[2];
+      if (0xCCU == send_mode)
+        {
+          Eigen::Matrix3d R;
+          for (unsigned int i = 0; i < 3; i++)
+            for (unsigned int j = 0; j < 3; j++)
+              R(i,j) = M[j*3+i];
+          Eigen::Quaternion<double> q(R);
+          msg.orientation.w = (double)q.w();// q(0);
+          msg.orientation.x = (double)q.x();// q(1);
+          msg.orientation.y = (double)q.y();// q(2);
+          msg.orientation.z = (double)q.z();// q(3);
+        }
       pub.publish(msg);
     }
 
   // Stop continous and close device
-  boost::asio::write(*serial_port, boost::asio::buffer(stop, 3));
-  ROS_WARN("Wait 0.1s"); 
-  ros::Duration(0.1).sleep();
-  serial_port->close(); 
+  if (!set_mode)
+    {
+      boost::asio::write(*serial_port, boost::asio::buffer(stop, 3));
+      ROS_WARN("Wait 0.1s");
+      ros::Duration(0.1).sleep();
+      serial_port->close();
+    }
 
-  return 0;
+  return EXIT_SUCCESS;
 }
